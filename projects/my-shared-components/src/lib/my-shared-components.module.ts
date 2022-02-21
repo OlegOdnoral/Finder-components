f3540163bbcd1adeac5493bea667a55cdd438d5c
Oleg <4uiiakabpa@gmail.com>
2022-02-21 23:56:00 +0200

    Add lib

---
+++ projects/my-shared-components/src/lib/my-shared-components.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { MySharedComponentsComponent } from './my-shared-components.component';
+
+
+
+@NgModule({
+  declarations: [
+    MySharedComponentsComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    MySharedComponentsComponent
+  ]
+})
+export class MySharedComponentsModule { }
