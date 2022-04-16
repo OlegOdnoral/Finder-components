b70ef2141288a1092ca503140ae10ae0a265c628
Oleg Odnoral <4uiiakabpa@gmail.com>
2022-04-16 12:01:23 +0300

    Selector

--- src/app/core/finder-selector/finder-selector.component.ts
+++ src/app/core/finder-selector/finder-selector.component.ts
@@ -2,21 +2,25 @@ import {
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
-  EmbeddedViewRef, EventEmitter,
-  Input, NgZone,
+  ElementRef,
+  EmbeddedViewRef,
+  EventEmitter,
+  Input,
   OnDestroy,
-  OnInit, Output, TemplateRef,
+  OnInit,
+  Output,
+  TemplateRef,
   ViewContainerRef
 } from '@angular/core';
-import { debounceTime, filter, fromEvent, takeUntil } from "rxjs";
+import { debounceTime, merge, Observable, Subscription } from "rxjs";
 import { FormControl } from "@angular/forms";
-import Popper from 'popper.js';
 import { Overlay, OverlayRef } from "@angular/cdk/overlay";
+import { TemplatePortal } from "@angular/cdk/portal";
 
 @Component({
   selector: 'finder-selector',
   templateUrl: './finder-selector.component.html',
-  styleUrls: ['./finder-selector.component.scss'],
+  styleUrls: [ './finder-selector.component.scss' ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FinderSelectorComponent implements OnInit, OnDestroy {
@@ -30,27 +34,26 @@ export class FinderSelectorComponent implements OnInit, OnDestroy {
   @Output() selectChange = new EventEmitter();
   @Output() closed = new EventEmitter();
 
-  visibleOptions = 4;
+  private dropdownClosingActionsSub = Subscription.EMPTY;
+  private isDropdownOpen = false;
+
+  visibleOptions = 5;
   searchControl = new FormControl();
 
   // @ts-ignore
   private view: EmbeddedViewRef<any>;
   // @ts-ignore
-  private popperRef: any;
+  private popperRef: OverlayRef;
   private originalOptions = [];
 
   constructor(
-    private vcr: ViewContainerRef,
-    private zone: NgZone,
-    private cdr: ChangeDetectorRef,
-    private overlay: Overlay
+    private readonly viewContainerRef: ViewContainerRef,
+    private changeDetectorRef: ChangeDetectorRef,
+    private elementRef: ElementRef<HTMLElement>,
+    private overlay: Overlay,
   ) {}
 
-  get isOpen() {
-    return !!this.popperRef;
-  }
-
-  ngOnInit() {
+  ngOnInit(): void {
     this.originalOptions = [ ...this.options ];
     if (this.model !== undefined) {
       this.model = this.options.find(currentOption => currentOption[this.idKey] === this.model);
@@ -61,76 +64,96 @@ export class FinderSelectorComponent implements OnInit, OnDestroy {
       .subscribe(term => this.search(term));
   }
 
-  get label() {
+  ngOnDestroy(): void {
+    if (this.popperRef) {
+      this.popperRef.dispose();
+    }
+  }
+
+  get isOpen(): boolean {
+    return this.isDropdownOpen;
+  }
+
+  get label(): string {
     return this.model ? this.model[this.labelKey] : 'Select...';
   }
 
-  open(dropdownTpl: TemplateRef<any>, origin: HTMLElement) {
-    this.view = this.vcr.createEmbeddedView(dropdownTpl);
-    const dropdown = this.view.rootNodes[0];
-
-    document.body.appendChild(dropdown);
-    dropdown.style.width = `${ origin.offsetWidth }px`;
-
-    this.zone.runOutsideAngular(() => {
-      this.popperRef = this.overlay.create({
-        hasBackdrop: false,
-        width: origin.offsetWidth
-      });
-      // this.popperRef = new Popper(origin, dropdown, {
-      //   removeOnDestroy: true
-      // });
-    });
-
-    this.handleClickOutside();
-  }
-
-  close() {
-    this.closed.emit();
-    // this.popperRef.destroy();
-    this.view.destroy();
-    this.searchControl.patchValue('');
-    // @ts-ignore
-    this.view = null;
-    // @ts-ignore
-    this.popperRef = null;
+  public open(dropdownTpl: TemplateRef<any>, origin: HTMLElement): void {
+    this.isDropdownOpen ? this.destroyDropdown() : this.openDropdown(dropdownTpl, origin);
   }
 
   // @ts-ignore
-  select(option) {
+  public select(option): void {
     this.model = option;
     this.selectChange.emit(option[this.idKey]);
     // the handleClickOutside function will close the dropdown
+    this.destroyDropdown();
   }
+
   // @ts-ignore
-  isActive(option) {
+  public isActive(option): boolean {
     if (!this.model) {
       return false;
     }
     return option[this.idKey] === this.model[this.idKey];
   }
 
-  search(value: string) {
+  public search(value: string): void {
     // @ts-ignore
     this.options = this.originalOptions.filter(option => option[this.labelKey].includes(value));
     requestAnimationFrame(() => (this.visibleOptions = this.options.length || 1));
   }
 
-  private handleClickOutside() {
-    fromEvent(document, 'click')
-      .pipe(
-        filter(({ target }) => {
-          const origin = this.popperRef.hostElement;
-          return origin.contains(target as HTMLElement) === false;
-        }),
-        takeUntil(this.closed)
-      )
-      .subscribe(() => {
-        this.close();
-        this.cdr.detectChanges();
-      });
+  private openDropdown(dropdownTpl: TemplateRef<any>, origin: HTMLElement): void {
+    this.isDropdownOpen = true;
+    this.popperRef = this.overlay.create({
+      hasBackdrop: true,
+      width: origin.offsetWidth,
+      backdropClass: 'cdk-overlay-transparent-backdrop',
+      positionStrategy: this.overlay
+        .position()
+        .flexibleConnectedTo(this.elementRef)
+        .withPositions([
+          {
+            originX: 'start',
+            originY: 'bottom',
+            overlayX: 'start',
+            overlayY: 'top',
+            offsetY: 8,
+            offsetX: 4
+          }
+        ])
+    });
+
+    const templatePortal = new TemplatePortal(
+      dropdownTpl,
+      this.viewContainerRef
+    );
+    this.popperRef.attach(templatePortal);
+
+    this.dropdownClosingActionsSub = this.dropdownClosingActions()
+      .subscribe(() => this.destroyDropdown());
+
+    this.changeDetectorRef.markForCheck();
   }
 
-  ngOnDestroy() {}
+  private dropdownClosingActions(): Observable<MouseEvent | unknown> {
+    const backdropClick$ = this.popperRef.backdropClick();
+    const detachment$ = this.popperRef.detachments();
+
+    return merge(backdropClick$, detachment$);
+  }
+
+  private destroyDropdown(): void {
+    if (!this.popperRef || !this.isDropdownOpen) {
+      return;
+    }
+
+    this.dropdownClosingActionsSub.unsubscribe();
+    this.isDropdownOpen = false;
+    this.popperRef.detach();
+
+    this.changeDetectorRef.markForCheck();
+  }
 
 }
