ae5e0bef16e649a7e4660df57c43827bc3d653d8
Oleg Odnoral <4uiiakabpa@gmail.com>
2022-04-20 01:35:23 +0300

    update select box

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   @Input() label = 'Default';
   @Input() disabled = false;
 
   @Output() click: EventEmitter<void> =  new EventEmitter();
+
+  control = new FormControl();
 
   selected = 'option2';
 
@@ -17,6 +20,10 @@ export class AppComponent {
     id: i,
     label: `Item #${i}`
   })) as any;
+
+  ngOnInit() {
+    this.control.valueChanges.subscribe(data => console.log(data));
+  }
 
   change($event: any) {
     console.log($event)
--- src/app/core/finder-selector/finder-selector.component.ts
+++ src/app/core/finder-selector/finder-selector.component.ts
@@ -3,16 +3,16 @@ import {
   ChangeDetectorRef,
   Component,
   ElementRef,
-  EventEmitter,
+  EventEmitter, forwardRef, HostBinding, HostListener,
   Input,
   OnDestroy,
   OnInit,
   Output,
-  TemplateRef,
+  TemplateRef, ViewChild,
   ViewContainerRef
 } from '@angular/core';
-import { merge, Observable, Subject, Subscription, takeUntil } from "rxjs";
-import { FormControl } from "@angular/forms";
+import { debounceTime, merge, Observable, Subject, Subscription, takeUntil } from "rxjs";
+import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from "@angular/forms";
 import { Overlay, OverlayRef } from "@angular/cdk/overlay";
 import { TemplatePortal } from "@angular/cdk/portal";
 
@@ -20,9 +20,18 @@ import { TemplatePortal } from "@angular/cdk/portal";
   selector: 'finder-selector',
   templateUrl: './finder-selector.component.html',
   styleUrls: [ './finder-selector.component.scss' ],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.Default,
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => FinderSelectorComponent),
+    multi: true
+  }]
 })
-export class FinderSelectorComponent implements OnInit, OnDestroy {
+export class FinderSelectorComponent implements OnInit, OnDestroy, ControlValueAccessor {
+
+  value = 0;
+  private onChange = (value: any) => {};
+  private onTouched = () => {};
 
   @Input() model: any;
   @Input() labelKey = 'label';
@@ -31,13 +40,16 @@ export class FinderSelectorComponent implements OnInit, OnDestroy {
   @Input() disabled = false;
   @Input() optionTpl!: TemplateRef<any>;
 
-  @Output() selectChange = new EventEmitter();
+  @Output() selectionChange = new EventEmitter();
   @Output() closed = new EventEmitter();
+
+  @ViewChild('origin') origin!: ElementRef<HTMLInputElement>;
+  @ViewChild('dropdown') dropdown!: TemplateRef<any>
 
   public visibleOptions = 5;
   public searchControl = new FormControl();
 
-  private popperRef!: OverlayRef;
+  private overlayRef!: OverlayRef;
   private dropdownClosingActionsSub = Subscription.EMPTY;
   private isDropdownOpen = false;
   private originalOptions: any[] = [];
@@ -70,21 +82,46 @@ export class FinderSelectorComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    if (this.popperRef) {
-      this.popperRef.dispose();
+    if (this.overlayRef) {
+      this.overlayRef.dispose();
     }
     this.destroyed.next(null);
     this.destroyed.complete()
   }
 
-  public open(dropdownTpl: TemplateRef<any>, origin: HTMLElement): void {
-    this.isDropdownOpen ? this.destroyDropdown() : this.openDropdown(dropdownTpl, origin);
+  registerOnChange(fn: any) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this.onTouched = fn;
+  }
+
+  writeValue(value: any) {
+    this.value = value;
+    this.onChange(this.value);
+  }
+
+  public open(): void {
+    if (this.isDropdownOpen) {
+      return
+    }
+    this.openDropdown(this.dropdown, this.origin.nativeElement)
+  }
+
+  public close(): void {
+    this.destroyDropdown();
+  }
+
+  public toggle(): void {
+    this.isDropdownOpen ? this.close() : this.open();
   }
 
   public select(option: any): void {
     this.model = option;
-    this.selectChange.emit(option[this.valueKey]);
-    this.destroyDropdown();
+    this.selectionChange.emit(option);
+    this.close();
+    this.writeValue(option);
   }
 
   public isActive(option: any): boolean {
@@ -101,10 +138,8 @@ export class FinderSelectorComponent implements OnInit, OnDestroy {
 
   private openDropdown(dropdownTpl: TemplateRef<any>, origin: HTMLElement): void {
     this.isDropdownOpen = true;
-    this.popperRef = this.overlay.create({
-      hasBackdrop: true,
+    this.overlayRef = this.overlay.create({
       width: origin.offsetWidth,
-      backdropClass: 'cdk-overlay-transparent-backdrop',
       positionStrategy: this.overlay
         .position()
         .flexibleConnectedTo(this.elementRef)
@@ -124,31 +159,31 @@ export class FinderSelectorComponent implements OnInit, OnDestroy {
       dropdownTpl,
       this.viewContainerRef
     );
-    this.popperRef.attach(templatePortal);
+    this.overlayRef.attach(templatePortal);
 
     this.dropdownClosingActionsSub = this.dropdownClosingActions()
-      .subscribe(() => this.destroyDropdown());
+      .subscribe(() =>  this.close());
 
     this.changeDetectorRef.markForCheck();
   }
 
   private dropdownClosingActions(): Observable<MouseEvent | unknown> {
-    const backdropClick$ = this.popperRef.backdropClick();
-    const detachment$ = this.popperRef.detachments();
+    const outsidePointerEvents$ = this.overlayRef.outsidePointerEvents()
+    const detachment$ = this.overlayRef.detachments();
 
-    return merge(backdropClick$, detachment$);
+    return merge(outsidePointerEvents$, detachment$).pipe(debounceTime(1));
   }
 
   private destroyDropdown(): void {
-    if (!this.popperRef || !this.isDropdownOpen) {
+    if (!this.overlayRef || !this.isDropdownOpen) {
       return;
     }
 
     this.dropdownClosingActionsSub.unsubscribe();
+    this.overlayRef.detach();
     this.isDropdownOpen = false;
-    this.popperRef.detach();
 
     this.changeDetectorRef.markForCheck();
   }
 
-}
+  }
